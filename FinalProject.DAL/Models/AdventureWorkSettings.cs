5a1bbe5e744485797401541f16ed512e8113b117
Sincere Walker <sincerewalker@Sinceres-MacBook-Air.local>
2023-11-07 16:12:15 -0500

    About page adding Sincere's picture

---
+++ FinalProject.DAL/Models/AdventureWorkSettings.cs
@@ -0,0 +1,9 @@
+namespace FinalProject.DAL.Models
+{
+    public class AdventureWorkSettings
+{
+    public string categoryUrl { get; set; } = string.Empty;
+    public string productUrl { get; set; } = string.Empty;
+    public string suppliersUrl { get; set; } = string.Empty;
+    }
+    }
